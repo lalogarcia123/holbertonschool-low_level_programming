70aa08c2c1366cb67f7765bf9ab233c4e8b7c69c
lalogarcia123 <lalogarcia123@gmail.com>
2024-02-23 01:52:16 +0000

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Funcion printf pruebas
+ *
+ * Return: devuelve 0???
+ *
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art", \n);
+return (0);
+}

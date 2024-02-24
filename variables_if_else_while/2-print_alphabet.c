dbbb8322eab5d4366167acd74f79e37b309ea53a
lalogarcia123 <lalogarcia123@gmail.com>
2024-02-24 03:16:25 +0000

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - putchar
+ *
+ * Return: que es esto?
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}

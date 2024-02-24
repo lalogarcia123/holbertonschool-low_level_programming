9b484d7919f886e657f72ca150cd85fe389ef3c0
lalogarcia123 <lalogarcia123@gmail.com>
2024-02-24 03:24:10 +0000

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - lo mismo pero con otro tipo de dato int
+  *
+  * Return: 
+  */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
